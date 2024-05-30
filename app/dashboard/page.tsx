8161100592bf662dfee2757ce0def231bde491a3
tomas <tomy2002.tf@gmail.com>
2024-05-30 11:12:12 -0300

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return <p>Dashboard Page</p>
+};
+
+//Para crear una ruta nueva, creamos una carpeta dentro de la carpeta app, dentro de la misma creamos un archivo page.tsx
+
+// la ruta en el navegador seria .../"nombre de la carpeta"
